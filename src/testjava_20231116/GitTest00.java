e7cae2e0e7599e0d6a1011e8bbb833a5d3a95884
won15367 <won15367@gmail.com>
2023-11-20 11:26:20 +0900

    깃허브 테스트

---
+++ src/testjava_20231116/GitTest00.java
@@ -0,0 +1,11 @@
+package testjava_20231116;
+
+public class GitTest00 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("깃허브 테스트");
+
+	}
+
+}
